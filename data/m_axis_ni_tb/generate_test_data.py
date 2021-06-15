d8463621d2d693e8948e07c594416b3e86f86f28
yangyt96 <yeeyang.tan@live.com>
2021-06-15 11:01:57 +0200

    [feat] module for testbench that generates signal for master axis network interface

---
+++ data/m_axis_ni_tb/generate_test_data.py
@@ -0,0 +1,116 @@
+import os
+import math
+
+# ----------------------------------------------
+# General function
+
+
+def bit_width(x: int):
+    if x > 1:
+        return math.ceil(math.log2(x))
+    elif x == 1:
+        return 1
+    else:
+        return 0
+
+
+def int_to_bin(val, len):
+    return bin(val)[2:].zfill(len)
+
+
+def to_file(path, str_obj):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "w") as handle:
+        handle.write(str_obj)
+        handle.close()
+
+
+# ----------------------------------------------
+# NoC configuration
+max_x_dim = 2  # check noc pkg
+max_y_dim = 2  # check noc pkg
+max_z_dim = 1  # check noc pkg
+
+flit_size = 32  # const
+flit_pkt_width = 5  # check noc pkg
+flit_addr_x_width = bit_width(max_x_dim)
+flit_addr_y_width = bit_width(max_y_dim)
+flit_addr_z_width = bit_width(max_z_dim)
+flit_id_width = 8
+flit_pad_width = flit_size - flit_id_width - flit_pkt_width \
+    - 2 * (flit_addr_x_width + flit_addr_y_width + flit_addr_z_width)
+
+# Router configuration
+port_num = 5  # check noc pkg
+
+port_exist = [x for x in range(1, port_num)]
+max_pkt_len = 2**flit_pkt_width - 1
+
+# ----------------------------------------------
+# Generated files: same in vhdl
+fname_inj_time = "inj_time.txt"
+fname_pkt_len = "pkt_len.txt"
+fname_flit_data = "flit_data.txt"
+
+# ----------------------------------------------
+# Functions
+
+
+def conv_id_to_coord(router_id):
+    z = router_id // (max_x_dim * max_y_dim)
+    y = router_id % (max_x_dim * max_y_dim) // max_x_dim
+    x = router_id % (max_x_dim * max_y_dim) % max_x_dim
+
+    return {"x": x, "y": y, "z": z}
+
+
+def create_flit_hdr(pad, id, src, dst, pkt_len):
+    src_coord = conv_id_to_coord(src)
+    dst_coord = conv_id_to_coord(dst)
+
+    hdr = ""
+    hdr += int_to_bin(pad, flit_pad_width)
+    hdr += int_to_bin(id, flit_id_width)
+    hdr += int_to_bin(src_coord["z"], flit_addr_z_width)
+    hdr += int_to_bin(src_coord["y"], flit_addr_y_width)
+    hdr += int_to_bin(src_coord["x"], flit_addr_x_width)
+    hdr += int_to_bin(dst_coord["z"], flit_addr_z_width)
+    hdr += int_to_bin(dst_coord["y"], flit_addr_y_width)
+    hdr += int_to_bin(dst_coord["x"], flit_addr_x_width)
+    hdr += int_to_bin(pkt_len, flit_pkt_width)
+
+    assert len(hdr) == flit_size
+    return hdr
+
+
+if __name__ == "__main__":
+    # ----------------------------------------------
+
+    # port 1
+    port_val = 1
+    time = 5
+    flit_data = []
+    inj_time = []
+    pkt_len = []
+
+    for itr, j in enumerate(range(30, 32)):
+        flit = create_flit_hdr(pad=0, id=itr, src=port_val, dst=0, pkt_len=j)
+
+        flit_data.extend([flit for itr in range(j)])
+        inj_time.append(time)
+        pkt_len.append(j)
+        time += 30
+
+    print(flit_data)
+    print(inj_time)
+    print(pkt_len)
+
+    assert len(flit_data) == sum(pkt_len)
+
+    path_inj_time = os.path.join("in", str(port_val), fname_inj_time)
+    path_pkt_len = os.path.join("in", str(port_val), fname_pkt_len)
+    path_flit_data = os.path.join("in", str(port_val), fname_flit_data)
+
+    to_file(path_inj_time, "\n".join(map(str, inj_time)))
+    to_file(path_pkt_len, "\n".join(map(str, pkt_len)))
+    to_file(path_flit_data, "\n".join(flit_data))
---
+++ testbench/utils/generator_router_local.vhd
@@ -0,0 +1,100 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_misc.all;
+use ieee.numeric_std.all;
+
+use work.NOC_3D_PACKAGE.all;
+use work.TESTBENCH_PACKAGE.all;
+
+entity generator_router_local is
+    generic (
+        Xis                : Integer           := 0;
+        Yis                : Integer           := 0;
+        Zis                : Integer           := 0;
+        port_num           : Integer           := 5;
+        port_exist         : integer_vec       := (0, 1, 2, 3, 4);
+        vc_num_vec         : integer_vec       := (2, 2, 2, 2, 2);
+        vc_num_out_vec     : integer_vec       := (2, 2, 2, 2, 2);
+        vc_depth_array     : vc_prop_int_array := ((2, 2), (2, 2), (2, 2), (2, 2), (2, 2));
+        vc_depth_out_array : vc_prop_int_array := ((32, 32), (2, 2), (2, 2), (2, 2), (2, 2));
+        rout_algo          : String            := "XYZ"
+    );
+    port (
+        clk : in Std_logic;
+        rst : in Std_logic;
+
+        o_local_tx          : out flit;
+        o_local_vc_write_tx : out Std_logic_vector(vc_num_out_vec(0) - 1 downto 0);
+        i_local_incr_rx     : in Std_logic_vector(vc_num_vec(0) - 1 downto 0)
+    );
+end entity;
+
+architecture behave of generator_router_local is
+    -- Router port signals
+    signal router_data_rx         : flit_vector(port_num - 1 downto 0)                         := (others => (others => '0'));
+    signal router_vc_write_rx_vec : Std_logic_vector(int_vec_sum(vc_num_vec) - 1 downto 0)     := (others => '0');
+    signal router_incr_rx_vec     : Std_logic_vector(int_vec_sum(vc_num_out_vec) - 1 downto 0) := (others => '0');
+    signal router_data_tx         : flit_vector(port_num - 1 downto 0)                         := (others => (others => '0'));
+    signal router_vc_write_tx_vec : Std_logic_vector(int_vec_sum(vc_num_out_vec) - 1 downto 0) := (others => '0');
+    signal router_incr_tx_vec     : Std_logic_vector(int_vec_sum(vc_num_vec) - 1 downto 0);--     := (others => '0');
+
+begin
+    -- I/O connections
+    o_local_tx                                     <= router_data_tx(0);
+    o_local_vc_write_tx                            <= router_vc_write_tx_vec(vc_num_out_vec(0) - 1 downto 0);
+    router_incr_rx_vec(vc_num_vec(0) - 1 downto 0) <= i_local_incr_rx;
+
+    -- instance
+    inst_router : entity work.router_pl
+        generic map(
+            port_num                     => port_num,
+            Xis                          => Xis,
+            Yis                          => Yis,
+            Zis                          => Zis,
+            header_incl_in_packet_length => true,
+            -- integer vector of range "0 to port_num-1"
+            port_exist     => port_exist,
+            vc_num_vec     => vc_num_vec,
+            vc_num_out_vec => vc_num_out_vec,
+            -- integer vector of range "0 to port_num-1, 0 to max_vc_num-1"
+            vc_depth_array     => vc_depth_array,
+            vc_depth_out_array => vc_depth_out_array,
+            rout_algo          => rout_algo
+        )
+        port map(
+            -- System
+            clk => clk,
+            rst => rst,
+            -- Inputs
+            data_rx         => router_data_rx,
+            vc_write_rx_vec => router_vc_write_rx_vec,
+            incr_rx_vec     => router_incr_rx_vec,
+            -- Outputs
+            data_tx_pl         => router_data_tx,
+            vc_write_tx_pl_vec => router_vc_write_tx_vec,
+            incr_tx_pl_vec     => router_incr_tx_vec
+        );
+
+    -- input
+    gen_inst_traffic : for i in 1 to port_num - 1 generate
+    begin
+        inst_traffic : entity work.traffic_gen
+            generic map(
+                flit_width          => flit_size,
+                router_credit       => vc_depth_array(i)(0),
+                srl_fifo_depth      => 200,
+                inj_time_text       => "data/m_axis_ni_tb/in/" & Integer'image(i) & "/inj_time.txt",  -- r
+                packet_length_text  => "data/m_axis_ni_tb/in/" & Integer'image(i) & "/pkt_len.txt",   -- r
+                image_2_flits_text  => "data/m_axis_ni_tb/in/" & Integer'image(i) & "/flit_data.txt", -- r
+                inj_time_2_noc_text => "data/m_axis_ni_tb/out/" & Integer'image(i) & "/inj_time.txt"  -- w
+            )
+            port map(
+                clk      => clk,
+                rst      => rst,
+                valid    => router_vc_write_rx_vec(i * vc_num_vec(0)),
+                incr     => router_incr_tx_vec(i * vc_num_vec(0)),
+                data_out => router_data_rx(i)
+            );
+    end generate;
+
+end architecture;
