77c51f70055ad74d69ab6b5a7473ecb5e218f195
yangyt96 <yeeyang.tan@live.com>
2021-07-05 12:33:58 +0200

    [chore] naming convention

--- source/axi/m_axis_ni.vhd
+++ source/axi/m_axis_ni.vhd
@@ -64,9 +64,9 @@ architecture implementation of M_AXIS_NI is
 
     signal tlast_counter : Integer;
 
-    signal fifo_read_en    : Std_logic_vector(VC_NUM - 1 downto 0);
-    signal fifo_read_valid : Std_logic_vector(VC_NUM - 1 downto 0);
-    signal fifo_data_out   : flit_vector(VC_NUM - 1 downto 0);
+    signal fifos_read_en    : Std_logic_vector(VC_NUM - 1 downto 0);
+    signal fifos_read_valid : Std_logic_vector(VC_NUM - 1 downto 0);
+    signal fifos_data_out   : flit_vector(VC_NUM - 1 downto 0);
 
     signal taddr     : Integer range 0 to VC_NUM - 1;
     signal init_flag : Std_logic;
@@ -80,21 +80,21 @@ architecture implementation of M_AXIS_NI is
 
 begin
     -- I/O connection
-    o_local_incr_tx_vec <= fifo_read_en;
+    o_local_incr_tx_vec <= fifos_read_en;
 
-    M_AXIS_TVALID <= '1' when state = s_WORK and fifo_read_valid(taddr) = '1' and tlast_counter > 0 else
+    M_AXIS_TVALID <= '1' when state = s_WORK and fifos_read_valid(taddr) = '1' and tlast_counter > 0 else
         '0';
-    M_AXIS_TDATA <= fifo_data_out(taddr);
+    M_AXIS_TDATA <= fifos_data_out(taddr);
     M_AXIS_TSTRB <= (others => '1');
     M_AXIS_TLAST <= '1' when tlast_counter = 1 else
         '0';
 
     -- Internal signal
-    fifo_read_en <= shift_vc when state = s_WORK and fifo_read_valid(taddr) = '1' and tlast_counter > 0 and M_AXIS_TREADY = '1' else
+    fifos_read_en <= shift_vc when state = s_WORK and fifos_read_valid(taddr) = '1' and tlast_counter > 0 and M_AXIS_TREADY = '1' else
         (others => '0');
 
     shift_vc  <= Std_logic_vector(shift_left(to_unsigned(1, shift_vc'length), taddr));
-    rotate_vc <= Std_logic_vector(rotate_right(unsigned(fifo_read_valid), taddr));
+    rotate_vc <= Std_logic_vector(rotate_right(unsigned(fifos_read_valid), taddr));
 
     -- determine the vc addr
     process (M_AXIS_ACLK, M_AXIS_ARESETN)
@@ -119,7 +119,7 @@ begin
         if M_AXIS_ARESETN = RST_LVL then
             clz_data <= (others => '0');
         elsif rising_edge(M_AXIS_ACLK) then
-            if state = s_IDLE and or_reduce(fifo_read_valid) = '1' then
+            if state = s_IDLE and or_reduce(fifos_read_valid) = '1' then
                 for i in 0 to VC_NUM - 1 loop
                     clz_data(i) <= rotate_vc(VC_NUM - 1 - i);
                 end loop;
@@ -137,7 +137,7 @@ begin
             case state is
 
                 when s_IDLE =>
-                    if or_reduce(fifo_read_valid) = '1' then
+                    if or_reduce(fifos_read_valid) = '1' then
                         state <= s_INIT;
                     end if;
 
@@ -168,8 +168,8 @@ begin
             tlast_counter <= 0;
         elsif rising_edge(M_AXIS_ACLK) then
             if state = s_INIT and init_flag = '1' then
-                tlast_counter <= to_integer(unsigned(get_header_inf(fifo_data_out(taddr)).packet_length));
-            elsif state = s_WORK and or_reduce(fifo_read_en) = '1' then
+                tlast_counter <= to_integer(unsigned(get_header_inf(fifos_data_out(taddr)).packet_length));
+            elsif state = s_WORK and or_reduce(fifos_read_en) = '1' then
                 tlast_counter <= tlast_counter - 1;
             end if;
         end if;
@@ -201,9 +201,9 @@ begin
                 data_in  => i_local_rx,
                 write_en => i_local_vc_write_rx(i),
 
-                data_out   => fifo_data_out(i),
-                read_en    => fifo_read_en(i),
-                read_valid => fifo_read_valid(i)
+                data_out   => fifos_data_out(i),
+                read_en    => fifos_read_en(i),
+                read_valid => fifos_read_valid(i)
             );
     end generate;
 
