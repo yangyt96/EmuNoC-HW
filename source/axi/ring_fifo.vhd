e1348692c1b00e94d81555119c9db3557f63798b
yangyt96 <yeeyang.tan@live.com>
2021-07-30 04:54:59 +0200

    [chore] change count down to count up

--- source/axi/ring_fifo.vhd
+++ source/axi/ring_fifo.vhd
@@ -11,10 +11,10 @@
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
 -- Description: Circular FIFO.
---              The data can written to the FIFO at rising_edge(clk), write_en='1'
---              and write_valid='1'.
---              The data can be read from the FIFO at rising_edge(clk), read_en='1'
---              and read_valid='1'.
+--              The data can written to the FIFO at rising_edge(clk), i_wen='1'
+--              and o_wvalid='1'.
+--              The data can be read from the FIFO at rising_edge(clk), i_ren='1'
+--              and o_rvalid='1'.
 -------------------------------------------------------------------------------
 -- Copyright (c) 2021
 -------------------------------------------------------------------------------
@@ -28,25 +28,28 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
+use work.NOC_3D_PACKAGE.all;
+
 entity ring_fifo is
     generic (
         BUFFER_DEPTH : Integer   := 6;
         DATA_WIDTH   : Integer   := 32;
+        CNT_WIDTH    : Integer   := bit_width(BUFFER_DEPTH);
         RST_LVL      : Std_logic := '0' -- reset level
     );
     port (
         clk : in Std_logic;
         rst : in Std_logic;
 
-        data_in     : in Std_logic_vector(DATA_WIDTH - 1 downto 0);
-        write_en    : in Std_logic;
-        write_valid : out Std_logic; -- whenever the buffer can be written data, this will always be 1
+        i_wdata  : in Std_logic_vector(DATA_WIDTH - 1 downto 0);
+        i_wen    : in Std_logic;
+        o_wvalid : out Std_logic; -- whenever the buffer can be written data, this will always be 1
 
-        read_en    : in Std_logic;
-        data_out   : out Std_logic_vector(DATA_WIDTH - 1 downto 0);
-        read_valid : out Std_logic; -- whenever the buffer contains data to be read, this will always be 1
+        i_ren    : in Std_logic;
+        o_rdata  : out Std_logic_vector(DATA_WIDTH - 1 downto 0);
+        o_rvalid : out Std_logic; -- whenever the buffer contains data to be read, this will always be 1
 
-        count : out Integer -- Indicate the amount of buffer can be written
+        count : out Std_logic_vector(CNT_WIDTH - 1 downto 0) -- Indicate the amount of buffer can be written
     );
 end entity ring_fifo;
 
@@ -59,13 +62,14 @@ architecture rtl of ring_fifo is
     signal counter       : Integer range 0 to BUFFER_DEPTH;
 
 begin
-    data_out <= memory(read_pointer) when counter /= BUFFER_DEPTH else
+    o_rdata <= memory(read_pointer) when counter /= 0 else
         (others => '0');
-    read_valid <= '1' when counter < BUFFER_DEPTH else
+    o_rvalid <= '1' when counter > 0 else
         '0';
-    write_valid <= '1' when counter > 0 else
+    o_wvalid <= '1' when counter < BUFFER_DEPTH else
         '0';
-    count <= counter;
+
+    count <= Std_logic_vector(to_unsigned(counter, CNT_WIDTH));
 
     process (clk, rst)
     begin
@@ -75,31 +79,31 @@ begin
             memory        <= (others => (others => '0'));
         elsif rising_edge(clk) then
 
-            if write_en = '1' and counter > 0 then
-                memory(write_pointer) <= data_in;
+            if i_wen = '1' and counter < BUFFER_DEPTH then
+                memory(write_pointer) <= i_wdata;
                 write_pointer         <= (write_pointer + 1) mod BUFFER_DEPTH;
             end if;
 
-            if read_en = '1' and counter < BUFFER_DEPTH then
+            if i_ren = '1' and counter > 0 then
                 read_pointer <= (read_pointer + 1) mod BUFFER_DEPTH;
             end if;
 
         end if;
     end process;
 
-    process (clk, rst, write_en, read_en)
+    process (clk, rst, i_wen, i_ren)
     begin
         if rst = RST_LVL then
-            counter <= BUFFER_DEPTH;
+            counter <= 0;
         elsif rising_edge(clk) then
-            if write_en = '1' and read_en = '0' and counter > 0 then
-                counter <= counter - 1;
-            elsif write_en = '0' and read_en = '1' and counter < BUFFER_DEPTH then
+            if i_wen = '1' and i_ren = '0' and counter < BUFFER_DEPTH then
                 counter <= counter + 1;
-            elsif write_en = '1' and read_en = '1' and counter = BUFFER_DEPTH then
+            elsif i_wen = '0' and i_ren = '1' and counter > 0 then
                 counter <= counter - 1;
-            elsif write_en = '1' and read_en = '1' and counter = 0 then
+            elsif i_wen = '1' and i_ren = '1' and counter = 0 then
                 counter <= counter + 1;
+            elsif i_wen = '1' and i_ren = '1' and counter = BUFFER_DEPTH then
+                counter <= counter - 1;
             end if;
         end if;
     end process;
