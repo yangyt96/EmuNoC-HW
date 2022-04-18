ddc2deebb0234f4a6376ddadc4641f6a8da2c3d6
yangyt96 <yeeyang.tan@live.com>
2022-04-18 09:15:57 +0200

    [chore] update test bench for clock halter

--- testbench/clock_halter_tb.vhd
+++ testbench/clock_halter_tb.vhd
@@ -17,10 +17,10 @@ architecture behave of clock_halter_tb is
     signal clk_cnt : Integer   := 0;
 
     -- Constants
-    constant CNT_WIDTH : Integer := 64;
+    constant CNT_WIDTH : Integer := 32;
 
     -- Signals
-    signal clk_halt  : Std_logic;
+    signal clkh      : Std_logic;
     signal ubcnt_wen : Std_logic;
     signal ubcnt     : Std_logic_vector(CNT_WIDTH - 1 downto 0);
     signal runcnt    : Std_logic_vector(CNT_WIDTH - 1 downto 0);
@@ -28,24 +28,74 @@ architecture behave of clock_halter_tb is
     signal out_halt  : Std_logic;
 
 begin
-    process (clk, rst)
+    -- process (clk, rst)
+    -- begin
+    --     if rising_edge(clk) then
+
+    --         if (clk_cnt mod 15) = 0 then
+    --             ubcnt     <= (others => '1');
+    --             ubcnt_wen <= '1';
+    --         else
+    --             ubcnt     <= (others => '0');
+    --             ubcnt_wen <= '0';
+    --         end if;
+
+    --         if (clk_cnt mod 3) = 0 then
+    --             in_halt <= '1';
+    --         else
+    --             in_halt <= '0';
+    --         end if;
+    --     end if;
+    -- end process;
+
+    process
     begin
-        if rising_edge(clk) then
 
-            if (clk_cnt mod 15) = 0 then
-                ubcnt     <= (others => '1');
-                ubcnt_wen <= '1';
-            else
-                ubcnt     <= (others => '0');
-                ubcnt_wen <= '0';
-            end if;
+        ubcnt     <= Std_logic_vector(to_unsigned(0, ubcnt'length));
+        ubcnt_wen <= '0';
+        in_halt   <= '0';
+        wait until rst /= RST_LVL;
 
-            if (clk_cnt mod 3) = 0 then
-                in_halt <= '1';
-            else
-                in_halt <= '0';
-            end if;
-        end if;
+        ubcnt     <= Std_logic_vector(to_unsigned(5, ubcnt'length));
+        ubcnt_wen <= '1';
+        in_halt   <= '0';
+        wait until clk_cnt = 1;
+
+        ubcnt     <= Std_logic_vector(to_unsigned(0, ubcnt'length));
+        ubcnt_wen <= '0';
+        in_halt   <= '0';
+        wait until clk_cnt = 10;
+
+        ubcnt     <= Std_logic_vector(to_unsigned(10, ubcnt'length));
+        ubcnt_wen <= '1';
+        in_halt   <= '0';
+        wait until clk_cnt = 11;
+
+        wait until clk_cnt = 12;
+        in_halt <= '1';
+        wait until clk_cnt = 13;
+        in_halt <= '0';
+
+        ubcnt     <= Std_logic_vector(to_unsigned(0, ubcnt'length));
+        ubcnt_wen <= '0';
+        in_halt   <= '0';
+        wait until clk_cnt = 20;
+
+        ubcnt     <= Std_logic_vector(to_unsigned(0, ubcnt'length));
+        ubcnt_wen <= '1';
+        in_halt   <= '0';
+        wait until clk_cnt = 21;
+
+        ubcnt     <= Std_logic_vector(to_unsigned(0, ubcnt'length));
+        ubcnt_wen <= '0';
+        in_halt   <= '0';
+        wait until clk_cnt = 30;
+
+        ubcnt     <= Std_logic_vector(to_unsigned(5, ubcnt'length));
+        ubcnt_wen <= '1';
+        in_halt   <= '0';
+        wait until clk_cnt = 31;
+
     end process;
 
     DUT : entity work.clock_halter
@@ -59,7 +109,7 @@ begin
 
             i_halt => in_halt,
             o_halt => out_halt,
-            clkh   => clk_halt,
+            clkh   => clkh,
 
             i_ub_count_wen => ubcnt_wen,
             i_ub_count     => ubcnt,
